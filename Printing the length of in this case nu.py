ee869335417960da13780dfe4cd88471552c9805
Eric5825 <90713132+Eric5825@users.noreply.github.com>
2022-04-17 19:18:11 -0400

    Add files via upload

---
+++ Printing the length of in this case nu.py
@@ -0,0 +1,3 @@
+# Printing the length of in this case numbers
+numbers = [1,2,3,4,5]
+print(len(numbers))
---
+++ clear all items in a listl.py
@@ -0,0 +1,4 @@
+# List methonds (delete all  items in a list), 
+numbers = [1,2,3,4,5]
+numbers.clear() #clear removes all items in a list..
+print(numbers)
